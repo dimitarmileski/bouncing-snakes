6bc5a04197df62c38d67c38fd0ae5380e0a26760
Dimitar Mileski <35492418+dimitarmileski@users.noreply.github.com>
2018-01-23 16:42:00 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,79 +1,81 @@
-let bubbles= [];
-
+let snake;
+let snakes = [];
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 
-	for(let i = 0; i < 50; i++){
-		bubbles[i] = new Bubble(random(windowWidth), random(windowHeight));
+	for(let i=0; i<10; i++){
+	 snakes[i] = new Snake();
 	}
-
 }
 
 function draw() {
-
 	background(255);
 
-	for(let b of bubbles){
-		b.show();
-		b.move();
 
-		let overlapping = false;
-
-		for(let other of bubbles)
-			if(b!=other && b.intersect(other))
-				overlapping = true;
-
-
-			if(overlapping)
-				b.changeColor(255);
-			else
-				b.changeColor(0);
-		
+	for(let i=0; i<snakes.length; i++){
+		snakes[i].display();
+		snakes[i].move();
+		snakes[i].bounce();
 	}
 	
+
 }
 
-class Bubble{
-	constructor(tempX, tempY){
-		this.x = tempX;
-		this.y = tempY;
-		this.speedX = 4;
-		this.speedY = 4;
-		this.width = 50;
-		this.height = 50;
-		this.bubbleRed = 0;
-		this.brightness = 0;
-	}
+class Snake{
+	constructor(){
+		let tempSize = random(5, 50);
 
-	show(){
-		fill(this.brightness, 0, 0);
-		noStroke();
-		ellipse(this.x, this.y, this.width, this.height);
+		this.x = random(windowWidth);
+		this.y = random(windowHeight);
+		this.width = tempSize;
+		this.height = tempSize;
+		this.speedX = 5;
+		this.speedY = 5;
+		this.color = random(5, 200);
+
+		this.history = [];
+	}	
+
+	display(){
+		fill(255);
+		ellipse(this.x, this.y,	this.width, this.height);
+
+
+		/*/////////////////////////////////*/
+
+		let v = createVector(this.x, this.y);
+		this.history.push(v);
+
+		/*/////////////////////////////////*/
+
+		if(this.history.length>20){
+			this.history.splice(0, 1);
+		}
+
+
+		for(let i=0; i<this.history.length; i++){
+			var position = this.history[i];
+			fill(0, this.color, 0);
+			noStroke();
+			ellipse(position.x + random(-5, 5), position.y + random(-5, 5), (this.width/2)+i, (this.height/2)+i);
+		}
+
+
 
 	}
 
 	move(){
-		this.x += random(-this.speedX, this.speedX);
-		this.y += random(-this.speedY, this.speedY);
-	}
-	
-	changeColor(brightness){
-		this.brightness = brightness;
+		this.x += this.speedX;
+		this.y += this.speedY;
 	}
 
 	bounce(){
-		if(this.x > windowWidth || this.x < 0)
+		if(this.x>windowWidth || this.x<0){
 			this.speedX *= -1;
+		}
 
-		if(this.y > windowHeight || this.y < 0)
+		if(this.y>windowHeight || this.y<0){
 			this.speedY *= -1;
+		}
 	}
-
-	intersect(secondObj){
-		let d = dist(this.x, this.y, secondObj.x, secondObj.y );
-
-		return(d < this.width/2 + secondObj.width/2);
-	}
-
-	
 }
